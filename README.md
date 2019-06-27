3008c1fb2bcf5a42855932083ecf38109223c14b
Patrick Jadwin <patrick_jadwin@discovery.com>
2019-06-27 14:40:26 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Erase Empty Folder
+
+To Run: `./erase_empty_folder.sh`
+
+## WARNING: DO NOT RUN AS SUDO/ROOT
+## THIS WILL RUN ON EVERY DIRECTORY AND SUB-DIRECTORY
---
+++ erase_empty_folder.sh
@@ -0,0 +1,18 @@
+printf "DO NOT RUN THIS WITH SUDO"
+printf "THIS WILL RUN ON EVERY DIRECTORY AND SUB-DIRECTORY"
+
+for D in `find . -type d`
+do
+    if [[ $D != "." ]]; then
+       if [[ $(ls $D -l | wc -l) -le 1 ]]; then
+            printf "\nDo you want to delete $D? ( Y / default (N) ): "
+            read res
+            if [[ $res == "Y" ]]; then
+                rm -rf $D
+                printf "\n$D IS DELETED\n"
+            else
+                printf "NOT DELETED"
+            fi
+        fi
+    fi
+done
